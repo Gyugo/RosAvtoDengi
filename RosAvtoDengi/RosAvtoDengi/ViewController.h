67cebb462f84f1a72dcc71f2149d294aa73345eb
Viktor Mishustin <mrgyugo@gmail.com>
2015-12-26 00:19:47 +0300

    Создание проекта

---
+++ RosAvtoDengi/RosAvtoDengi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RosAvtoDengi
+//
+//  Created by Viktor on 26.12.15.
+//  Copyright © 2015 Viktor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
